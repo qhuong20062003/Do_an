d39c3a48d37888bbb078e9c50bb8da1b129e5728
HoangHa2305 <hoangha2352@gmail.com>
2025-07-06 02:36:42 +0700

    update validate for client

--- resources/views/client/register.blade.php
+++ resources/views/client/register.blade.php
@@ -30,18 +30,21 @@
                 </div>
                 @endif
 
-                <form action="{{ route('post.register') }}" method="POST">
+                <form action="{{ route('post.register') }}" method="POST" id="myForm">
                     <p>
                         <label>Tên khách hàng <span class="required">*</span></label>
-                        <input type="text" name="name" placeholder="Nhập họ tên" required>
+                        <input type="text" name="name" id="name" placeholder="Nhập họ tên">
+                        <small id="name-error" class="text-danger" style="display: none;">Vui lòng nhập họ tên</small>
                     </p>
                     <p>
                         <label>Email <span class="required">*</span></label>
-                        <input type="email" name="email" placeholder="Nhập email" required>
+                        <input type="email" name="email" id="email" placeholder="Nhập email">
+                        <small id="email-error" class="text-danger" style="display: none;">Vui lòng nhập email</small>
                     </p>
                     <p>
                         <label>Mật khẩu <span class="required">*</span></label>
-                        <input type="password" name="password" placeholder="Nhập mật khẩu" required>
+                        <input type="password" name="password" id="password" placeholder="Nhập mật khẩu">
+                        <small id="password-error" class="text-danger" style="display: none;">Vui lòng nhập password</small>
                     </p>
                     <div class="login_submit">
                         <button type="submit">Đăng ký</button>
@@ -54,4 +57,46 @@
         <!--register area end-->
     </div>
 </div>
+
+<script>
+    $(document).ready(function(){
+        $('#myForm').on('submit', function(e) {
+            let is_valid = true;
+
+            let email = $('#email');
+            if(!email.val().trim()) {
+                $('#email-error').show();
+                email.css('border', '1px solid red');
+                is_valid = false;
+            } else {
+                $('#email-error').hide();
+                email.css('border', '');
+            }
+            
+            let password = $('#password');
+            if(!password.val().trim()) {
+                $('#password-error').show();
+                password.css('border', '1px solid red');
+                is_valid = false;
+            } else {
+                $('#password-error').hide();
+                password.css('border', '');
+            }
+
+            let name = $('#name');
+            if(!name.val().trim()) {
+                $('#name-error').show();
+                name.css('border', '1px solid red');
+                is_valid = false;
+            } else {
+                $('#name-error').hide();
+                name.css('border', '');
+            }
+
+            if(!is_valid) {
+                e.preventDefault();
+            }
+        });
+    });
+</script>
 @endsection
